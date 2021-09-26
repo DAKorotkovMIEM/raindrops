7dd5bf476dd49463f92b8b443a0851cf089781f0
DAKorotkovMIEM <90389685+DAKorotkovMIEM@users.noreply.github.com>
2021-09-26 23:59:43 +0300

    Update raindrops.py

--- raindrops.py
+++ raindrops.py
@@ -1,8 +1,11 @@
 def convert(number):
-    """В возврящаемую строку дописываются:
-'Pling', если число number делится на 3,
-'Plang', если число number делится на 5,
-'Plong', если число number делится на 7,
-само число number, если оно не делится ни на 3, ни на 5, ни на 7."""
-    # Напишите ваш код здесь
-    return str(number) # эту строку можно удалить
+    a = ''
+    if number % 3 == 0:
+        a += 'Pling'
+    if number % 5 == 0:
+        a += 'Plang'
+    if number % 7 == 0:
+        a += 'Plong'
+    if a == '':
+        a = str(number)
+    return a
